11a0ac488bb03084e00b6e2407986fbd09e80bec
TropheusJ <jverrellijr@icloud.com>
2024-05-04 10:24:53 -0400

    WIP task-based remapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.quiltmc.quilt_gradle.remapping.RemapTransform
-
 plugins {
     id("java")
     id("org.quiltmc.quilt-gradle")
@@ -11,15 +9,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven { setUrl("https://maven.quiltmc.org/repository/release") }
+    maven("https://maven.fabricmc.net")
+    maven("https://maven.quiltmc.org/repository/release")
 }
 
 dependencies {
     mappings {
         // declare all needed mappings
-        addMappings("obfuscated", "mojmap", files("client.txt"))
+//        addMappings("obfuscated", "mojmap", files("client.txt"))
         addMappings("obfuscated", "intermediary", "net.fabricmc:intermediary:1.20.4")
-        addMappings("intermediary", "quilt-mappings", "org.quiltmc:quilt-mappings:1.20.4+build.3:intermediary-v2")
+//        addMappings("intermediary", "quilt-mappings", "org.quiltmc:quilt-mappings:1.20.4+build.3:intermediary-v2")
         // choose target
         setTargetMappings("mojmap")
         // declare mapped dependencies
